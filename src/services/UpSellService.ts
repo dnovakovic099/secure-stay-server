ed99d4f34ac9e6a0b0989d9ec8bb7c852c18b38a
tribikram <sentribikram@gmail.com>
2024-02-19 11:45:43 +0545

    update: add listing in upsells

--- src/services/UpSellService.ts
+++ src/services/UpSellService.ts
@@ -1,29 +1,74 @@
-import { DeepPartial, Like } from "typeorm";
+import { Like } from "typeorm";
 import { UpSellEntity } from "../entity/UpSell";
 import { appDatabase } from "../utils/database.util";
 import { Request, Response } from "express";
+import { UpSellListing } from "../entity/UpSellListing";
+import { Listing } from "../entity/Listing";
 
 export class UpSellServices {
 
     private upSellRepository = appDatabase.getRepository(UpSellEntity)
+    private upSellListings = appDatabase.getRepository(UpSellListing)
+    private listingInfoRepository = appDatabase.getRepository(Listing)
 
     async saveUpSellInfo(request: Request, response: Response) {
         try {
-            const upSellInfo: DeepPartial<UpSellEntity> = request.body
-            await this.upSellRepository.save(upSellInfo)
+            const { listingIds, ...upSellInfo } = request.body
+
+            await appDatabase.transaction(async (transactionalEntityManager) => {
+
+                await transactionalEntityManager.save(UpSellEntity, upSellInfo)
+                //saving listing to associated upSell
+                await Promise.all(listingIds.map(async (listingId: number) => {
+                    const upSellListing = new UpSellListing();
+                    upSellListing.listingId = listingId;
+                    upSellListing.upSellId = upSellInfo.upSellId;
+                    upSellListing.status = 1;
+                    await transactionalEntityManager.save(upSellListing);
+                }));
+            })
             response.status(200).send({ "message": "Data saved successfully!!!" })
         } catch (error) {
-            console.log(error)
+            response.send(error)
         }
     }
 
     async updateUpSellInfo(request: Request, response: Response) {
         try {
-            const upSellInfo = request.body
-            await this.upSellRepository.update(upSellInfo.upSellId, upSellInfo)
-            response.status(200).send({
-                "message": "Data updated successfully!!!"
+            const { listingIds, ...upSellInfo } = request.body
+            //check for existing upsell
+            const data = await this.upSellRepository.find({
+                where: {
+                    status: 1,
+                    upSellId: upSellInfo.upSellId
+                }
             })
+            if (data.length == 0) {
+                response.send("No associated upsell found.")
+            } else {
+
+                await appDatabase.transaction(async (transactionalEntityManager) => {
+                    // Update UpSellListing status to 0
+                    await transactionalEntityManager.update(UpSellListing, { upSellId: upSellInfo.upSellId }, { status: 0 });
+
+                    // Update UpSellEntity
+                    await transactionalEntityManager.update(UpSellEntity, upSellInfo.upSellId, upSellInfo);
+
+                    // Save new UpSellListing records
+                    await Promise.all(listingIds.map(async (listingId: number) => {
+                        const upSellListing = new UpSellListing();
+                        upSellListing.listingId = listingId;
+                        upSellListing.upSellId = upSellInfo.upSellId;
+                        upSellListing.status = 1;
+                        await transactionalEntityManager.save(upSellListing);
+                    }));
+                })
+
+                response.status(200).send({
+                    "message": "Data updated successfully!!!"
+                })
+            }
+
 
         } catch (error) {
             console.log(error);
@@ -56,16 +101,78 @@ export class UpSellServices {
     async deleteUpSellInfo(request: Request, response: Response) {
         try {
             const upsell_id: any = request.query.upSellId
-            let upSellToUpdate = await this.upSellRepository.find({
+
+            //check either upsell is present in the table
+            const data = await this.upSellRepository.find({
                 where: {
+                    status: 1,
                     upSellId: upsell_id
                 }
             })
-            upSellToUpdate[0].status = 0
-            await this.upSellRepository.update(upsell_id, upSellToUpdate[0])
-            response.status(202).send({
-                "message": "Data deleted successfully!!!"
+
+            if (data.length == 0) {
+                response.send("No associated upsell found.")
+            } else {
+
+                await appDatabase.transaction(async (transactionalEntityManager) => {
+                    let upSellToUpdate = await transactionalEntityManager.find(UpSellEntity, {
+                        where: {
+                            upSellId: upsell_id
+                        }
+                    })
+
+                    await transactionalEntityManager.update(UpSellListing, { upSellId: upsell_id }, { status: 0 });
+
+                    upSellToUpdate[0].status = 0
+                    await transactionalEntityManager.update(UpSellEntity, upsell_id, upSellToUpdate[0])
+
+                })
+
+                response.status(202).send({
+                    "message": "Data deleted successfully!!!"
+                })
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async getUpSellAssociatedListing(request: Request, response: Response) {
+        try {
+
+            const upSellId: any = request.query.upSellId
+            //check for existing upsell
+            const data = await this.upSellRepository.find({
+                where: {
+                    status: 1,
+                    upSellId: upSellId
+                }
             })
+
+            if (data.length == 0) {
+                response.send("No associated upsell found.")
+            } else {
+
+                let upSellListing: any[] = []
+                let listingData = await this.upSellListings.find({
+                    where: {
+                        upSellId: upSellId,
+                        status: 1
+                    },
+                })
+
+                await Promise.all(listingData.map(async (data: any) => {
+                    const listingsInfo: any = await this.listingInfoRepository.find({
+                        where: { listingId: data.listingId }
+                    });
+                    upSellListing.push(listingsInfo)
+                }))
+
+                response.status(200).send(upSellListing);
+            }
+
+
         } catch (error) {
             console.log(error);
         }
