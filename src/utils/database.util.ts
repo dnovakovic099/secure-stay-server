003e6f4037f688f16c3a401382e27239567499f2
Prasanna <prasannakb440@gmail.com>
2024-02-24 21:02:07 +0545

    reverted the changes made in db-util

--- src/utils/database.util.ts
+++ src/utils/database.util.ts
@@ -1,12 +1,12 @@
 import { DataSource } from "typeorm";
 
 export const appDatabase = new DataSource({
-    type: "mysql",
+    type: "mariadb",
     host: process.env.DATABASE_URL,
     port: Number(process.env.DATABASE_PORT),
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
-    database: "boardingpass",
+    database: "boarding-pass",
     synchronize: true,
     entities: ["src/entity/*.ts"],
     subscribers: ["src/subscriber/*.ts"],
