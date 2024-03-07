d4692cfbfc4161845cfc3565ecee886991f0fbba
Prasanna <prasannakb440@gmail.com>
2024-03-07 21:57:26 +0545

    feature/added-messaging-phone-apis

--- src/controllers/MessagingController.ts
+++ src/controllers/MessagingController.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { MessagingService } from "../services/MessagingServices";
-import { dataSaved, dataUpdated } from "../helpers/response";
+import { dataDeleted, dataSaved, dataUpdated } from "../helpers/response";
 
 export class MessagingController {
     async saveEmailInfo(request: Request, response: Response, next: NextFunction) {
@@ -23,7 +23,46 @@ export class MessagingController {
             const { id } = request.params;
             await messagingService.deleteEmailInfo(Number(id));
 
-            return response.status(200).json(dataUpdated('Email deleted successfully'));
+            return response.status(200).json(dataDeleted('Email deleted successfully'));
+        } catch (error) {
+            return next(error);
+        }
+    }
+
+    async savePhoneNoInfo(request: Request, response: Response, next: NextFunction) {
+        try {
+            const messagingService = new MessagingService();
+
+            const { countryCode, phoneNo, supportsSMS, supportsCalling, supportsWhatsApp } = request.body;
+            await messagingService.savePhoneNoInfo(countryCode, phoneNo, supportsSMS, supportsCalling, supportsWhatsApp);
+
+            return response.status(201).json(dataSaved('Phone number saved successfully'));
+        } catch (error) {
+            return next(error);
+        }
+    }
+
+    async deletePhoneNoInfo(request: Request, response: Response, next: NextFunction) {
+        try {
+            const messagingService = new MessagingService();
+
+            const { id } = request.params;
+            await messagingService.deletePhoneNoInfo(Number(id));
+
+            return response.status(200).json(dataUpdated('Phone number deleted successfully'));
+        } catch (error) {
+            return next(error);
+        }
+    };
+
+    async updatePhoneNoInfo(request: Request, response: Response, next: NextFunction) {
+        try {
+            const messagingService = new MessagingService();
+
+            const { id, countryCode, phoneNo, supportsSMS, supportsCalling, supportsWhatsApp } = request.body;
+            await messagingService.updatePhoneNoInfo(id, countryCode, phoneNo, supportsSMS, supportsCalling, supportsWhatsApp);
+
+            return response.status(201).json(dataDeleted('Phone number info updated successfully'));
         } catch (error) {
             return next(error);
         }
--- src/router/messagingRoutes.ts
+++ src/router/messagingRoutes.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express';
-import { validateSaveEmailInfoRequest } from '../middleware/validation/messaging.validation';
+import { validateSaveEmailInfoRequest, validateSavePhoneNoInfoRequest, validateUpdatePhoneNoInfoRequest } from '../middleware/validation/messaging.validation';
 import { MessagingController } from '../controllers/MessagingController';
 
 const router = Router();
 const messagingController = new MessagingController();
 
 router.route('/saveemailinfo').post(validateSaveEmailInfoRequest, messagingController.saveEmailInfo);
-
 router.route('/deleteemailinfo/:id').delete(messagingController.deleteEmailInfo);
+
+router.route('/savephonenoinfo').post(validateSavePhoneNoInfoRequest, messagingController.savePhoneNoInfo);
+router.route('/deletephonenoinfo/:id').delete(messagingController.deletePhoneNoInfo);
+router.route('/updatephonenoinfo').put(validateUpdatePhoneNoInfoRequest, messagingController.updatePhoneNoInfo);
 
 export default router
 
