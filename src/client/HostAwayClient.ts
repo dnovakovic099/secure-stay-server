c484ee7a916e78da5e3b929983777c28656d709e
Prasanna Ku. Baniya <prasannakb440@gmail.com>
2025-01-08 00:08:31 +0545

    updated getReservation method

--- src/client/HostAwayClient.ts
+++ src/client/HostAwayClient.ts
@@ -189,7 +189,7 @@ export class HostAwayClient {
     channelId: number | ""
   ): Promise<Object[]> {
     
-    let url = `https://api.hostaway.com/v1/reservations?${dateType}StartDate=${startDate}&${dateType == "arrival" ? "departure" : "arrival"}EndDate=${endDate}&limit=${limit}&offset=${offset}&sortOrder=${dateType}DateDesc`;
+    let url = `https://api.hostaway.com/v1/reservations?${dateType}StartDate=${startDate}&${dateType}EndDate=${endDate}&limit=${limit}&offset=${offset}&sortOrder=${dateType}DateDesc`;
 
     if (listingId) {
       url += `&listingId=${listingId}`;
