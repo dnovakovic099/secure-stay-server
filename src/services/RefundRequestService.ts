49c7ea62887b7d4a85396c8f7a582ccf9ccecaab
Prasanna Ku. Baniya <prasannakb440@gmail.com>
2025-03-08 18:26:02 +0545

    updated the email receipient

--- src/services/RefundRequestService.ts
+++ src/services/RefundRequestService.ts
@@ -311,9 +311,9 @@ export class RefundRequestService {
 
         // Send email to admin for all refund requests
         if (refundRequests.length == 1) {
-          await this.sendSingleRefundRequestEmail("admin@luxurylodgingpm.com", refundRequests[0], currentTimeStamp); // Call function for a single request
+          await this.sendSingleRefundRequestEmail(process.env.EMAIL_TO, refundRequests[0], currentTimeStamp); // Call function for a single request
         } else {
-          await this.sendMultipleRefundRequestsEmail("admin@luxurylodgingpm.com", refundRequests, currentTimeStamp);
+          await this.sendMultipleRefundRequestsEmail(process.env.EMAIL_TO, refundRequests, currentTimeStamp);
         }
 
         // Send email to users based on the number of requests they have
