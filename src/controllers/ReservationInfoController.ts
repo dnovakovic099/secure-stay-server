efc09aa8b636659704baa3070d1cf1137b11f507
Prasanna Ku. Baniya <prasannakb440@gmail.com>
2025-03-25 13:57:06 +0545

    added api to fetch reservation by id

--- src/controllers/ReservationInfoController.ts
+++ src/controllers/ReservationInfoController.ts
@@ -2,19 +2,39 @@ import { NextFunction, Request, Response } from "express";
 import { ReservationInfoService } from "../services/ReservationInfoService";
 
 export class ReservationInfoController {
-    async getAllReservations(request: Request, response: Response) {
-        const reservationInfoService = new ReservationInfoService();
-        const result = await reservationInfoService.getReservationInfo(request);
-        if (result.status === "error") {
-            return response.status(500).send(result);
+    async getAllReservations(request: Request, response: Response, next: NextFunction) {
+        try {
+            const reservationInfoService = new ReservationInfoService();
+            const result = await reservationInfoService.getReservationInfo(request);
+            if (result.status === "error") {
+                return response.status(500).send(result);
+            }
+            return response.send(result);
+        } catch (error) {
+            return next(error);
         }
-        return response.send(result);
     }
 
-    async exportReservationToExcel(request: Request, response: Response) {
-        const reservationInfoService = new ReservationInfoService();
-        return response.send(await reservationInfoService.exportReservationToExcel(request));
+    async getReservation(request: Request, response: Response, next: NextFunction) {
+        try {
+            const reservationInfoService = new ReservationInfoService();
+            const reservationId = Number(request.params.reservationId);
+            const result = await reservationInfoService.getReservationById(reservationId);
+            return response.send(result);
+        } catch (error) {
+            return next(error);
+        }
+    }
+
+    async exportReservationToExcel(request: Request, response: Response, next: NextFunction) {
+        try {
+            const reservationInfoService = new ReservationInfoService();
+            return response.send(await reservationInfoService.exportReservationToExcel(request));
+        } catch (error) {
+            return next(error);
+        }
     }    
+    
     async syncReservations(request: Request, response: Response, next: NextFunction) {
         try {
             const reservationInfoService = new ReservationInfoService();
