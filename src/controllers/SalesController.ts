7acd755e445b1970bea2d04d6eec2543219a36c9
Emran <emranyonas602@gmail.com>
2025-03-29 18:42:24 +0300

    fix: ssid

--- src/controllers/SalesController.ts
+++ src/controllers/SalesController.ts
@@ -574,7 +574,7 @@ export class SalesController {
         return response.json({
           success: true,
           ...apiResponse.data.payload,
-          screenshotSessionId,
+         ssid:screenshotSessionId,
         });
       }
       return response.status(404).json({
