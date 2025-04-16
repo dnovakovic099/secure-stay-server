5e2f78c795ad48f591b1134f3abed8f9f3bf4f6b
Prasanna Ku. Baniya <prasannakb440@gmail.com>
2025-04-16 17:35:27 +0545

    added logs in the upsell webhook

--- src/controllers/ChargeAutomationWebhookController.ts
+++ src/controllers/ChargeAutomationWebhookController.ts
@@ -1,15 +1,17 @@
 import { Request, Response } from 'express';
 import { ChargeAutomationWebhookService } from '../services/ChargeAutomationWebhookService';
+import logger from '../utils/logger.utils';
 
 export class ChargeAutomationWebhookController {
     
     async handleWebhook(req: Request, res: Response) {
         const webhookService = new ChargeAutomationWebhookService();
         try {
+            logger.error('[ChargeAutomationWebhookController][handleWebhook] Data received from webhook:', JSON.stringify(req.body));
             const result = await webhookService.processWebhook(req.body);
             return res.status(200).json(result);
         } catch (error) {
-            console.error('Webhook processing error:', error);
+            logger.error('[ChargeAutomationWebhookController][handleWebhook] Webhook processing error:', error);
             return res.status(500).json({ 
                 status: 'error',
                 message: 'Internal server error'
