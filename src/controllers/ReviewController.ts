e783ec051989be8c883b4aa3fbea6752e5c83481
Prasanna Ku. Baniya <prasannakb440@gmail.com>
2025-06-24 23:18:56 +0545

    filter changes in get review API

--- src/controllers/ReviewController.ts
+++ src/controllers/ReviewController.ts
@@ -11,9 +11,9 @@ export class ReviewController {
     async getReviews(request: CustomRequest, response: Response, next: NextFunction) {
         try {
             const reviewService = new ReviewService();
-            const { fromDate, toDate, listingId, page, limit, rating, owner, claimResolutionStatus, status, isClaimOnly ,reviewerNameHeaderSort, listingHeaderSort, arrivalDateHeaderSort, departureDateHeaderSort, guestNameHeaderSort, channelHeaderSort, ratingHeaderSort } = request.query;
+            const { fromDate, toDate, listingId, page, limit, rating, owner, claimResolutionStatus, status, isClaimOnly  } = request.query;
 
-            const { reviewList, totalCount } = await reviewService.getReviews({ fromDate, toDate, listingId, page, limit, rating, owner, claimResolutionStatus, status, isClaimOnly, reviewerNameHeaderSort, listingHeaderSort, arrivalDateHeaderSort, departureDateHeaderSort, guestNameHeaderSort, channelHeaderSort, ratingHeaderSort });
+            const { reviewList, totalCount } = await reviewService.getReviews({ fromDate, toDate, listingId, page, limit, rating, owner, claimResolutionStatus, status, isClaimOnly });
 
             return response.status(200).json({
                 success: true,
--- src/middleware/validation/reviews/review.validation.ts
+++ src/middleware/validation/reviews/review.validation.ts
@@ -9,21 +9,16 @@ export const validateGetReviewRequest = (request: Request, response: Response, n
         toDate: Joi.string()
             .pattern(/^\d{4}-\d{2}-\d{2}$/)
             .messages({ 'string.pattern.base': 'toDate must be in the format "yyyy-mm-dd"' }).optional(),
-        listingId: Joi.number().optional(),
+        listingId: Joi.array().items(
+            Joi.number().required()
+        ).min(1).required().allow(null, ""),
         page: Joi.number().required(),
         limit: Joi.number().required(),
         rating: Joi.number().max(10).min(0).optional(),
-        owner: Joi.string().required().optional(),
+        owner: Joi.array().items(Joi.string().required()).min(1).required().allow(null, ""),
         claimResolutionStatus: Joi.string().optional().valid("N/A", "Pending", "Completed", "Denied"),
         isClaimOnly: Joi.boolean().optional(),
-        status: Joi.string().required().valid("active", "hidden"),
-        reviewerNameHeaderSort: Joi.string().valid('ASC', 'DESC', null).optional(),
-        listingHeaderSort: Joi.string().valid('ASC', 'DESC', null).optional(),
-        arrivalDateHeaderSort: Joi.string().valid('ASC', 'DESC', null).optional(),
-        departureDateHeaderSort: Joi.string().valid('ASC', 'DESC', null).optional(),
-        guestNameHeaderSort: Joi.string().valid('ASC', 'DESC', null).optional(),
-        channelHeaderSort: Joi.string().valid('ASC', 'DESC', null).optional(),
-        ratingHeaderSort: Joi.string().valid('ASC', 'DESC', null).optional(),
+        status: Joi.string().required().valid("active", "hidden").allow(null, ""),
     }).custom((value, helpers) => {
         if ((value?.fromDate && !value?.toDate) || (!value?.fromDate && value?.toDate)) {
             return helpers.message({ custom: 'Both fromDate and toDate must be provided together' });
