9b87ab8b265b411ecdcecfcb819319e1fd0542ed
Vitalic <0makovskyi@gmail.com>
2025-09-26 15:16:38 +0300

    added filter

--- src/controllers/PublishedStatementController.ts
+++ src/controllers/PublishedStatementController.ts
@@ -2,23 +2,36 @@ import { NextFunction, Request, Response } from "express";
 import { PublishedStatementService } from "../services/PublishedStatementService";
 
 export class PublishedStatementController {
-    async savePublishedStatementFromHA(request: Request, response: Response, next: NextFunction) {
-        try {
-            const publishedStatementService = new PublishedStatementService();
-            const result = await publishedStatementService.savePublishedStatement();
-            return response.status(200).json({ message: "Published statements saved successfully", data: result });
-        } catch (error) {
-            next(error);
-        }
+  async savePublishedStatementFromHA(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
+    try {
+      const publishedStatementService = new PublishedStatementService();
+      const result = await publishedStatementService.savePublishedStatement();
+      return response.status(200).json({
+        message: "Published statements saved successfully",
+        data: result,
+      });
+    } catch (error) {
+      next(error);
     }
+  }
 
-    async getPublishedStatements(request: Request, response: Response, next: NextFunction) {
-        try {
-            const publishedStatementService = new PublishedStatementService();
-            const statements = await publishedStatementService.getPublishedStatements();
-            return response.status(200).json({ data: statements });
-        } catch (error) {
-            next(error);
-        }
+  async getPublishedStatements(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
+    try {
+      const publishedStatementService = new PublishedStatementService();
+      const statements = await publishedStatementService.getPublishedStatements(
+        request
+      );
+      return response.status(200).json({ data: statements });
+    } catch (error) {
+      next(error);
     }
+  }
 }
--- src/services/PublishedStatementService.ts
+++ src/services/PublishedStatementService.ts
@@ -3,88 +3,150 @@ import { PublishedStatementEntity } from "../entity/PublishedStatements";
 import { getPeriodType } from "../helpers/date";
 import { appDatabase } from "../utils/database.util";
 import logger from "../utils/logger.utils";
+import { Request } from "express";
 
 export class PublishedStatementService {
-    private publishedStatementRepo = appDatabase.getRepository(PublishedStatementEntity);
-    private hostawayClient = new HostAwayClient();
+  private publishedStatementRepo = appDatabase.getRepository(
+    PublishedStatementEntity
+  );
+  private hostawayClient = new HostAwayClient();
 
-    async createPublishedStatement(data: Partial<PublishedStatementEntity>): Promise<PublishedStatementEntity> {
-        const newStatement = this.publishedStatementRepo.create(data);
-        return await this.publishedStatementRepo.save(newStatement);
+  async createPublishedStatement(
+    data: Partial<PublishedStatementEntity>
+  ): Promise<PublishedStatementEntity> {
+    const newStatement = this.publishedStatementRepo.create(data);
+    return await this.publishedStatementRepo.save(newStatement);
+  }
+
+  async getPublishedStatements(
+    request?: any
+  ): Promise<PublishedStatementEntity[]> {
+    try {
+      const statements = await this.publishedStatementRepo.find();
+
+      const { year, month, durationType } = request.query;
+
+      const transformedStatements = statements.map((statement) => ({
+        ...statement,
+        listingMapIds: statement.listingMapIds
+          ? JSON.parse(statement.listingMapIds)
+          : [],
+        url: `https://dashboard.hostaway.com/v3/owner-statements/${statement.statementId}`,
+        durationType: getPeriodType(statement.fromDate, statement.toDate),
+      }));
+
+      let filteredStatements = transformedStatements;
+
+      if (year) {
+        const yearNum = parseInt(year as string);
+        filteredStatements = filteredStatements.filter((statement) => {
+          const statementYear = parseInt(statement.fromDate.split("-")[0]);
+          return statementYear === yearNum;
+        });
+      }
+
+      if (month) {
+        const monthNum = parseInt(month as string);
+        filteredStatements = filteredStatements.filter((statement) => {
+          const statementMonth = parseInt(statement.fromDate.split("-")[1]);
+          return statementMonth === monthNum;
+        });
+      }
+
+      if (durationType) {
+        filteredStatements = filteredStatements.filter(
+          (statement) => statement.durationType === durationType.toUpperCase()
+        );
+      }
+
+      return filteredStatements;
+    } catch (error) {
+      logger.error(error);
+      throw error;
     }
+  }
 
-    async getPublishedStatements(): Promise<PublishedStatementEntity[]> {
-        const statements = await this.publishedStatementRepo.find();
-        const transformedStatements = statements.map(statement => ({
-            ...statement,
-            listingMapIds: statement.listingMapIds ? JSON.parse(statement.listingMapIds) : [],
-            url: `https://dashboard.hostaway.com/v3/owner-statements/${statement.statementId}`,
-            durationType: getPeriodType(statement.fromDate, statement.toDate),
-        }));
-        return transformedStatements;
+  async fetchPublishedStatementFromHA() {
+    const response = await this.hostawayClient.getOwnerStatements();
+    if (!response) {
+      logger.error("No response from HostAway API for owner statements.");
+      return [];
     }
+    const publishedStatements = response.filter(
+      (statement: any) => statement.status === "new"
+    );
+    //sort the publishedStatements in desc order by id
+    publishedStatements.sort((a: any, b: any) => b.id - a.id);
+    return publishedStatements;
+  }
 
-    async fetchPublishedStatementFromHA() {
-        const response = await this.hostawayClient.getOwnerStatements();
-        if (!response) {
-            logger.error("No response from HostAway API for owner statements.");
-            return [];
+  async fetchPublishedStatementByIdFromHA(statementId: number) {
+    const response = await this.hostawayClient.getOwnerStatementById(
+      statementId
+    );
+    if (!response) {
+      logger.error(
+        `No response from HostAway API for owner statement ID ${statementId}.`
+      );
+      return null;
+    }
+    return response;
+  }
+
+  async savePublishedStatement() {
+    const publishedStatements = await this.fetchPublishedStatementFromHA();
+    const existingStatements = await this.getPublishedStatements();
+    const existingStatementIds = existingStatements.map(
+      (statement) => statement.statementId
+    );
+    for (const statement of publishedStatements) {
+      if (!existingStatementIds.includes(statement.id)) {
+        logger.info(
+          `Processing new published statement with ID ${statement.id}...`
+        );
+
+        const statementInfo = await this.fetchPublishedStatementByIdFromHA(
+          statement.id
+        );
+
+        if (!statementInfo) {
+          logger.error(
+            `Failed to fetch detailed info for statement ID ${statement.id}. Skipping...`
+          );
+          continue;
         }
-        const publishedStatements = response.filter((statement: any) => statement.status === "new");
-        //sort the publishedStatements in desc order by id
-        publishedStatements.sort((a: any, b: any) => b.id - a.id);
-        return publishedStatements;
-    }
-
-    async fetchPublishedStatementByIdFromHA(statementId: number) {
-        const response = await this.hostawayClient.getOwnerStatementById(statementId);
-        if (!response) {
-            logger.error(`No response from HostAway API for owner statement ID ${statementId}.`);
-            return null;
+        const { filterParametersJson } = statementInfo;
+        if (!filterParametersJson) {
+          logger.error(
+            `No filterParametersJson found for statement ID ${statement.id}. Skipping...`
+          );
+          continue;
         }
-        return response;
+
+        const newStatementData: Partial<PublishedStatementEntity> = {
+          fromDate: filterParametersJson.fromDate,
+          toDate: filterParametersJson.toDate,
+          dateType: filterParametersJson.dateType,
+          listingMapIds: JSON.stringify(filterParametersJson.listingMapIds),
+          statementName: statementInfo.statementName,
+          statementId: statementInfo.id,
+          durationType: statementInfo.durationType,
+          grandTotal: statementInfo.grandTotalAmount,
+          propertyOwnerName: statementInfo.propertyOwnerName,
+          propertyOwnerPhone: statementInfo.propertyOwnerPhone,
+          createdBy: "system",
+        };
+
+        await this.createPublishedStatement(newStatementData);
+        logger.info(
+          `Published statement with ID ${statement.id} saved successfully.`
+        );
+      } else {
+        logger.info(
+          `Published statement with ID ${statement.id} already exists in the database.`
+        );
+      }
     }
-
-    async savePublishedStatement() {
-        const publishedStatements = await this.fetchPublishedStatementFromHA();
-        const existingStatements = await this.getPublishedStatements();
-        const existingStatementIds = existingStatements.map(statement => statement.statementId);
-        for (const statement of publishedStatements) {
-            if (!existingStatementIds.includes(statement.id)) {
-                logger.info(`Processing new published statement with ID ${statement.id}...`);
-
-                const statementInfo = await this.fetchPublishedStatementByIdFromHA(statement.id);
-
-                if (!statementInfo) {
-                    logger.error(`Failed to fetch detailed info for statement ID ${statement.id}. Skipping...`);
-                    continue;
-                }
-                const { filterParametersJson } = statementInfo;
-                if (!filterParametersJson) {
-                    logger.error(`No filterParametersJson found for statement ID ${statement.id}. Skipping...`);
-                    continue;
-                }
-
-                const newStatementData: Partial<PublishedStatementEntity> = {
-                    fromDate: filterParametersJson.fromDate,
-                    toDate: filterParametersJson.toDate,
-                    dateType: filterParametersJson.dateType,
-                    listingMapIds: JSON.stringify(filterParametersJson.listingMapIds),
-                    statementName: statementInfo.statementName,
-                    statementId: statementInfo.id,
-                    durationType: statementInfo.durationType,
-                    grandTotal: statementInfo.grandTotalAmount,
-                    propertyOwnerName: statementInfo.propertyOwnerName,
-                    propertyOwnerPhone: statementInfo.propertyOwnerPhone,
-                    createdBy: "system",
-                };
-
-                await this.createPublishedStatement(newStatementData);
-                logger.info(`Published statement with ID ${statement.id} saved successfully.`);
-            } else {
-                logger.info(`Published statement with ID ${statement.id} already exists in the database.`);
-            }
-        }
-        return;
-    }
+    return;
+  }
 }
