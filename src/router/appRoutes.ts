81d4628e71203a9b5fb830d3c2daaa2f1b35ed24
Prasanna Ku. Baniya <prasannakb440@gmail.com>
2026-02-27 18:24:44 +0545

    Updated Employee Schedule logic

--- src/router/appRoutes.ts
+++ src/router/appRoutes.ts
@@ -51,7 +51,6 @@ import itemSupplyRequestRoutes from "./itemSupplyRequestRoutes";
 import allServiceRequestRoutes from "./allServiceRequestRoutes";
 import assignedTaskRoutes from "./assignedTaskRoutes";
 import employeeRoutes from "./employeeRoutes";
-import employeeScheduleRoutes from "./employeeScheduleRoutes";
 
 const router = Router();
 
@@ -109,7 +108,6 @@ router.use('/item-supply-request', itemSupplyRequestRoutes);
 router.use('/service-requests', allServiceRequestRoutes);
 router.use('/assigned-tasks', assignedTaskRoutes);
 router.use('/employees', employeeRoutes);
-router.use('/employee-schedules', employeeScheduleRoutes);
 
 export default router;
 
