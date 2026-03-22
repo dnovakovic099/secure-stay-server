f4b39c9c13cde6073ed353bf9063b872c5e090ef
Prasanna Ku. Baniya <prasannakb440@gmail.com>
2026-03-22 10:09:25 +0545

    Bind controller methods to ensure proper context in review routes

--- src/router/reviewRoutes.ts
+++ src/router/reviewRoutes.ts
@@ -10,12 +10,12 @@ const reviewController = new ReviewController();
 const reviewDetailController = new ReviewDetailController();
 
 router.route('/')
-    .get(verifySession, validateGetReviewRequest, reviewController.getReviews)
-    .post(verifySession, validateSaveReview, reviewController.saveReview)
+    .get(verifySession, validateGetReviewRequest, reviewController.getReviews.bind(reviewController))
+    .post(verifySession, validateSaveReview, reviewController.saveReview.bind(reviewController))
 
-router.route('/syncreviews').get(reviewController.syncReviews);
+router.route('/syncreviews').get(reviewController.syncReviews.bind(reviewController));
 
-router.route('/reviewvisibility/:id').put(verifySession, validateUpdateReviewVisibilityStatusRequest, reviewController.updateReviewVisibility)
+router.route('/reviewvisibility/:id').put(verifySession, validateUpdateReviewVisibilityStatusRequest, reviewController.updateReviewVisibility.bind(reviewController))
 
 router
     .route('/reviewdetails/:reviewId')
@@ -24,29 +24,29 @@ router
     .get(verifySession, reviewDetailController.getReviewDetails);
 
 router.route('/reviewcheckout')
-    .get(verifySession, validateGetReviewForCheckout, reviewController.getReviewsForCheckout)
+    .get(verifySession, validateGetReviewForCheckout, reviewController.getReviewsForCheckout.bind(reviewController))
 
-router.route('/reviewcheckout/update').put(verifySession, validateUpdateReviewForCheckout, reviewController.updateReviewCheckout)
+router.route('/reviewcheckout/update').put(verifySession, validateUpdateReviewForCheckout, reviewController.updateReviewCheckout.bind(reviewController))
 
-router.route('/reviewcheckout/latest-update/create').post(verifySession,validateCreateLatestUpdate, reviewController.createReviewCheckoutUpdate);
+router.route('/reviewcheckout/latest-update/create').post(verifySession,validateCreateLatestUpdate, reviewController.createReviewCheckoutUpdate.bind(reviewController));
 
 router.route('/liveissues')
-    .get(verifySession, validateGetLiveIssues, reviewController.getLiveIssues)
-    .post(verifySession, validateCreateLiveIssue, reviewController.createLiveIssue)
+    .get(verifySession, validateGetLiveIssues, reviewController.getLiveIssues.bind(reviewController))
+    .post(verifySession, validateCreateLiveIssue, reviewController.createLiveIssue.bind(reviewController))
 
-router.route('/liveissues/update').put(verifySession, validateUpdateLiveIssue, reviewController.updateLiveIssue)
+router.route('/liveissues/update').put(verifySession, validateUpdateLiveIssue, reviewController.updateLiveIssue.bind(reviewController))
 
-router.route('/liveissues/latest-update/create').post(verifySession, validateCreateLiveIssueUpdate, reviewController.createLiveIssueUpdate)
+router.route('/liveissues/latest-update/create').post(verifySession, validateCreateLiveIssueUpdate, reviewController.createLiveIssueUpdate.bind(reviewController))
 
-router.route('/bad-review/update-status').put(verifySession, validateBadReviewUpdateStatus, reviewController.updateBadReviewStatus);
+router.route('/bad-review/update-status').put(verifySession, validateBadReviewUpdateStatus, reviewController.updateBadReviewStatus.bind(reviewController));
 
-router.route('/bad-review/update-fields').put(verifySession, reviewController.updateBadReviewFields);
+router.route('/bad-review/update-fields').put(verifySession, reviewController.updateBadReviewFields.bind(reviewController));
 
-router.route('/bad-review/latest-update/create').post(verifySession, validateBadReviewLatestUpdate, reviewController.createBadReviewUpdate);
+router.route('/bad-review/latest-update/create').post(verifySession, validateBadReviewLatestUpdate, reviewController.createBadReviewUpdate.bind(reviewController));
 
 router.route('/bad-review')
-    .get(verifySession, validateGetBadReview, reviewController.getBadReview)
+    .get(verifySession, validateGetBadReview, reviewController.getBadReview.bind(reviewController))
 
-router.route('/dashboard').get(verifySession, reviewController.getDashboardStats);
+router.route('/dashboard').get(verifySession, reviewController.getDashboardStats.bind(reviewController));
 
 export default router;
